3cf5a69f7b1888414e09893396e550146fe19234
Ahmad Ammari <ammariect@gmail.com>
2024-12-03 11:38:09 +0000

    test mongo data fetch

---
+++ tests/mongo/test_read.py
@@ -0,0 +1,7 @@
+from llm_engineering.domain.documents import ArticleDocument, UserDocument
+user = UserDocument.get_or_create(first_name="Paul", last_name="Iusztin")
+articles = ArticleDocument.bulk_find(author_id=str(user.id))
+print(f"User ID: {user.id}")
+print(f"User name: {user.first_name} {user.last_name}")
+print(f"Number of articles: {len(articles)}")
+print("First article link:", articles[0].link)
