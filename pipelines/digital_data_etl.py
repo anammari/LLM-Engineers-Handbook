c070e6d8b1d761f986dade0cf0cd262b72a194c6
Ahmad Ammari <ammariect@gmail.com>
2024-12-17 04:21:57 +0000

    updated changes

--- pipelines/digital_data_etl.py
+++ pipelines/digital_data_etl.py
@@ -2,7 +2,6 @@ from zenml import pipeline
 
 from steps.etl import crawl_links, get_or_create_user
 
-
 @pipeline
 def digital_data_etl(user_full_name: str, links: list[str]) -> str:
     user = get_or_create_user(user_full_name)
